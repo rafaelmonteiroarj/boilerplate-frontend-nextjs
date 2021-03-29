c823f18fb2c83484c74f0b2394d5a73ec1c6c8f6
Rafael Monteiro Arjonas <rafaelmonteiroarj@gmail.com>
2021-03-29 16:24:46 -0300

    :white_check_mark: Ajuste na configuraçao de testes, separando a configuraçao e teste unitario e end to end.
    Ajuste na configuraçao de testes, separando a configuraçao e teste unitario e end to end.

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,21 @@
+const { resolve } = require('path');
+
+const root = resolve(__dirname, '..');
+
 /** https://jestjs.io/docs/configuration */
 module.exports = {
-  testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+  rootDir: root /**  raiz do diretório que contém o arquivo de configuração do Jest */,
+  displayName:
+    'root-tests' /** Permite que uma etiqueta seja impressa ao lado de um teste durante sua execução. */,
+  testEnvironment:
+    'jsdom' /** O ambiente de teste que será usado para teste. */,
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/.next/'
+  ] /** Pastas ou arquivos a serem ignorados  */,
+  collectCoverage: true /** Indica que as informações de coleta do teste devem ser coletadas e reportadas no console. */,
+  collectCoverageFrom: [
+    'src/**/tests/*.ts(x)?',
+    '!src/**/stories/*.tsx'
+  ] /** Um padrão glob relativo à rootDircorrespondência dos arquivos dos quais as informações de cobertura precisam ser coletadas. */
 };
