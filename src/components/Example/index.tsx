42a73a6917e8d9040de8a790f4ee5fb9bf0230e7
Rafael Monteiro Arjonas <rafaelmonteiroarj@gmail.com>
2021-04-01 18:22:16 -0300

    :wrench: Adicionando componente de exemplo basico para o boilerplate.
    Adicionando componente de exemplo basico para o boilerplate.

---
+++ src/components/Example/index.tsx
@@ -0,0 +1,26 @@
+import * as S from './styles';
+
+const Example = ({
+  title = 'Boilerplate React',
+  description = 'TypeScript, ReactJS, NextJS e Styled Components'
+}: Props) => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado."
+    />
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela com código."
+    />
+  </S.Wrapper>
+);
+
+interface Props {
+  title?: string;
+  description?: string;
+}
+
+export default Example;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import Main from 'components/Main';
+import Example from 'components/Example';
 
 export default function Home() {
-  return <Main />;
+  return <Example />;
 }
