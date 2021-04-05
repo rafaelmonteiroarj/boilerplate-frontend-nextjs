d4495f97cdd2f1562fcc07e359e39f7223a4889b
Rafael Monteiro Arjonas <rafaelmonteiroarj@gmail.com>
2021-04-05 11:11:29 -0300

    :wrench: ajuste na configuraçao do jest.
    Inserçao do setupFilesAfterEnv (Para definir arquivo a ser executado antes dos tests).

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,9 @@ const root = resolve(__dirname);
 /** https://jestjs.io/docs/configuration */
 module.exports = {
   rootDir: root /**  raiz do diretório que contém o arquivo de configuração do Jest */,
+  setupFilesAfterEnv: [
+    '<rootDir>/.jest/setup.ts'
+  ] /** Uma lista de caminhos para módulos que executam algum código para configurar ou definir a estrutura de teste antes de cada arquivo de teste no conjunto ser executado.  */,
   displayName:
     'root-tests' /** Permite que uma etiqueta seja impressa ao lado de um teste durante sua execução. */,
   testMatch: [
@@ -19,6 +22,8 @@ module.exports = {
   collectCoverage: true /** Indica que as informações de coleta do teste devem ser coletadas e reportadas no console. */,
   collectCoverageFrom: [
     'src/**/*.ts(x)?',
-    '!src/**/stories/*.tsx'
+    '!src/**/stories/*.tsx',
+    '!src/pages/**/*.tsx',
+    '!src/styles/**/*.ts'
   ] /** Um padrão glob relativo à rootDircorrespondência dos arquivos dos quais as informações de cobertura precisam ser coletadas. */
 };
